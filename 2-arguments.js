16384e14a857648822416862c26c39715102a514
Melita <melitamiloka@gmail.com>
2025-11-09 19:24:32 +0100

    Add 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,7 @@
 const args = ProcessingInstruction.argv.slice(2);
-if (args.length === 0) { console.log('No argument');
-} else if (args.length === 1) { console.log('Argument found');
-} else { console.log('Arguments found'); }
+if (args.length === 0) { 
+    console.log('No argument');
+} else if (args.length === 1) { 
+    console.log('Argument found');
+} else { 
+    console.log('Arguments found'); }
