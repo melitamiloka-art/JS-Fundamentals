adaf7fc9b6d6522cb882249c5b734f163520414a
Melita <melitamiloka@gmail.com>
2025-11-11 15:44:41 +0100

    Add 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,9 @@
-const arg = ProcessingInstruction.argv[2];
-if (arg === undefined) {
-    console.log('No argument');
-} else {
-    console.log('arg');
+// This script print the first argument passed to it.
+const { argv } = require('node:process');
+const arg = argv[2];
+
+if(arg === undefined) {
+	console.log('No argument');
+}else{
+	console.log(arg);
 }
