722a5bfd6dc4423298df801e1fcdab6f4876c100
Melita <melitamiloka@gmail.com>
2025-11-11 16:29:04 +0100

    Add 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that pint out the value of an Array.
+
+const stringArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < stringArr.length; i++) {
+	console.log(stringArr[i])
+}
