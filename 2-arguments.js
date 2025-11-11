51514593521e256256c3358450cf8cb7657e7f0d
Melita <melitamiloka@gmail.com>
2025-11-11 15:31:41 +0100

    add 2-arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,10 @@
-const argv = require('node:process');
+const { argv } = require('node:process');
+
 const args = argv.slice(2);
-if (args.length === 0) {
-console.log('No argument');
-} else if (args.length === 1) {
-console.log('Argument found');
-} else { console.log('Arguments found'); }
+if(args.length === 0) {
+    console.log('No argument');
+}else if(args.length === 1) {
+    console.log('Argument found');
+}else {
+    console.log('Arguments found')
+}
